53ced9170d6fac83cabdd14b51585a8889b1bd3c
VanDuong279 <vanduong27904@gmail.com>
2023-11-10 21:18:34 +0700

    Nop bai thuc hanh

---
+++ ss6_javascript/index.js
@@ -0,0 +1,12 @@
+function Showmesseage(){
+    alert("Hello");
+}
+function Showmesseage1(){
+    document.getElementById("demo").innerHTML = "Xin Chao";
+}
+function Showmesseage2(){
+    document.write("xin chao")
+}
+function Showmesseage3() {
+    console.log("Xin Chao!")
+}
